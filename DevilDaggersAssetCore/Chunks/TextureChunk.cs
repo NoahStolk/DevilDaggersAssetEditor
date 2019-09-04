b51fd48a83fa7b3d002cfbe6d1c7027b3b080ab5
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-04 23:47:53 +0200

    Include LodePNG C++ code to call using P/Invoke for texture extractions (does not work yet).

--- DevilDaggersAssetCore/Chunks/TextureChunk.cs
+++ DevilDaggersAssetCore/Chunks/TextureChunk.cs
@@ -1,13 +1,27 @@
 ﻿using DevilDaggersAssetCore.Headers;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace DevilDaggersAssetCore.Chunks
 {
-	// TODO: Figure out how to encode/decode PNGs. (There is no working LodePNG for .NET unfortunately.)
 	public class TextureChunk : AbstractHeaderedChunk<TextureHeader>
 	{
+		private const string DllFilePath = @"..\..\..\LodePNG\Debug\LodePNG.dll"; // TODO: Include dll file in the project itself when textures are working.
+
+		[DllImport(DllFilePath, CallingConvention = CallingConvention.StdCall, ExactSpelling = false, CharSet = CharSet.Auto)]
+		private extern static uint lodepng_encode32(out byte[] outchars, out uint outsize, byte[] image, uint w, uint h);
+
 		public TextureChunk(string name, uint startOffset, uint size, uint unknown)
 			: base(name, startOffset, size, unknown)
 		{
+		}
+
+		public override IEnumerable<FileResult> Extract()
+		{
+			// TODO: Crashes with no errors whatsoever... Probably wrong signature.
+			lodepng_encode32(out byte[] newBytes, out _, Buffer, Header.Width, Header.Height);
+
+			yield return new FileResult(Name, newBytes);
 		}
 	}
 }
