7ac4365e68af5c1e72928d4cd1f5eed6df3207d5
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-11 22:44:58 +0200

    Update NuGet packages. First correct attempt at extracting textures. Images are flipped and the color format is wrong but other than that the textures look correct. Remove PNGEncodeDecodeTest project. Remove LodePNG library.

--- DevilDaggersAssetCore/Chunks/TextureChunk.cs
+++ DevilDaggersAssetCore/Chunks/TextureChunk.cs
@@ -1,31 +1,31 @@
 ﻿using DevilDaggersAssetCore.Headers;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace DevilDaggersAssetCore.Chunks
 {
 	public class TextureChunk : AbstractHeaderedChunk<TextureHeader>
 	{
-		// TODO: Build dll in x64.
-		// TODO: Include dll file in the project itself when textures are working.
-		private const string DllFilePath = @"..\..\..\LodePNG\Debug\LodePNG.dll";
-
-		[DllImport(DllFilePath, CallingConvention = CallingConvention.StdCall, ExactSpelling = false, CharSet = CharSet.Auto)]
-		private extern static uint lodepng_encode32(out byte[] outchars, out uint outsize, byte[] image, uint w, uint h);
-
 		public TextureChunk(string name, uint startOffset, uint size, uint unknown)
 			: base(name, startOffset, size, unknown)
 		{
 		}
 
-#if LODE_PNG
 		public override IEnumerable<FileResult> Extract()
 		{
-			// TODO: Crashes with no errors whatsoever... Probably wrong signature.
-			lodepng_encode32(out byte[] newBytes, out _, Buffer, Header.Width, Header.Height);
-
-			yield return new FileResult(Name, newBytes);
+			// Format32bppArgb
+			using Bitmap bitmap = new Bitmap((int)Header.Width, (int)Header.Height, (int)Header.Width * 4, PixelFormat.Format32bppRgb, Marshal.UnsafeAddrOfPinnedArrayElement(Buffer, 0));
+			yield return new FileResult(Name, GetBytes(bitmap));
 		}
-#endif
+
+		public static byte[] GetBytes(Bitmap image)
+		{
+			MemoryStream memoryStream = new MemoryStream();
+			new Bitmap(image).Save(memoryStream, ImageFormat.Png);
+			return memoryStream.ToArray();
+		}
 	}
 }
