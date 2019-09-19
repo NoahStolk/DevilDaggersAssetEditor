d11507fb77dfbe62bb4e2cf0edfa4f19c1812c53
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-19 21:09:33 +0200

    Check for new version on start up.

--- DevilDaggersAssetEditor/GUI/Windows/Main.xaml.cs
+++ DevilDaggersAssetEditor/GUI/Windows/Main.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DevilDaggersAssetCore;
 using DevilDaggersAssetEditor.Code.Web;
+using System.ComponentModel;
 using System.Windows;
 
 namespace DevilDaggersAssetEditor.GUI.Windows
@@ -18,11 +19,20 @@ namespace DevilDaggersAssetEditor.GUI.Windows
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
-			if (NetworkHandler.Instance.VersionResult.IsUpToDate.HasValue && !NetworkHandler.Instance.VersionResult.IsUpToDate.Value)
+			BackgroundWorker worker = new BackgroundWorker();
+			worker.DoWork += (object checkVersionSender, DoWorkEventArgs checkVersionE) =>
 			{
-				UpdateRecommendedWindow updateRecommendedWindow = new UpdateRecommendedWindow();
-				updateRecommendedWindow.ShowDialog();
-			}
+				NetworkHandler.Instance.RetrieveVersion();
+			};
+			worker.RunWorkerCompleted += (object checkVersionSender, RunWorkerCompletedEventArgs echeckVersionE) =>
+			{
+				if (NetworkHandler.Instance.VersionResult.IsUpToDate.HasValue && !NetworkHandler.Instance.VersionResult.IsUpToDate.Value)
+				{
+					UpdateRecommendedWindow updateRecommendedWindow = new UpdateRecommendedWindow();
+					updateRecommendedWindow.ShowDialog();
+				}
+			};
+			worker.RunWorkerAsync();
 		}
 	}
 }
