641689c46874c978edc17602ac8530e33648c8e6
Noah Stolk <noah.stolk1997@gmail.com>
2020-02-15 17:42:46 +0100

    Update version to 0.7.13.0

--- DevilDaggersAssetEditor/Properties/AssemblyInfo.cs
+++ DevilDaggersAssetEditor/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+﻿using log4net.Config;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
-using log4net.Config;
 
 [assembly: AssemblyTitle("Devil Daggers Asset Editor")]
 [assembly: AssemblyDescription("Devil Daggers Asset Editor is a tool that can be used to extract assets from the Devil Daggers resource files, as well as compressing them back to create mods.")]
@@ -13,6 +13,6 @@ using log4net.Config;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.7.12.0")]
-[assembly: AssemblyFileVersion("0.7.12.0")]
+[assembly: AssemblyVersion("0.7.13.0")]
+[assembly: AssemblyFileVersion("0.7.13.0")]
 [assembly: XmlConfigurator(ConfigFile = "log4net.config")]
