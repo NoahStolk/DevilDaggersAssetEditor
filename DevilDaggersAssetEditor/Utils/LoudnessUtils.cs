d64e7929955e83ffe9f7f280a71a9b7072512279
Noah Stolk <noah.stolk1997@gmail.com>
2020-10-07 21:14:00 +0200

    Fix loudness values not being set when creating a mod file after extracting an audio binary

--- DevilDaggersAssetEditor/Utils/LoudnessUtils.cs
+++ DevilDaggersAssetEditor/Utils/LoudnessUtils.cs
@@ -1,13 +1,22 @@
-﻿namespace DevilDaggersAssetEditor.Utils
+﻿using System;
+using System.Globalization;
+
+namespace DevilDaggersAssetEditor.Utils
 {
 	public static class LoudnessUtils
 	{
-		public static bool TryReadLoudnessLine(string line, out string assetName, out float loudness)
+		public static bool TryReadLoudnessLine(string line, out string? assetName, out float loudness)
 		{
 			try
 			{
-				assetName = line.Substring(0, line.IndexOf('=')).Trim();
-				loudness = float.Parse(line.Substring(line.IndexOf('=') + 1, line.Length - assetName.Length - 1));
+				line = line
+					.Replace(" ", string.Empty, StringComparison.InvariantCulture) // Remove spaces to make things easier.
+					.TrimEnd('.'); // Remove dots at the end of the line. (The original loudness file has one on line 154 for some reason...)
+
+				int equalsIndex = line.IndexOf('=', StringComparison.InvariantCulture);
+
+				assetName = line.Substring(0, equalsIndex);
+				loudness = float.Parse(line.Substring(equalsIndex + 1, line.Length - assetName.Length - 1), CultureInfo.InvariantCulture);
 				return true;
 			}
 			catch
