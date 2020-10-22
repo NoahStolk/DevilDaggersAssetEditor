d4f760c274d729557a3608d279b82cf84b401fd0
Noah Stolk <noah.stolk1997@gmail.com>
2020-10-22 12:22:54 +0200

    Restructure extension methods

--- DevilDaggersAssetEditor.Wpf/Extensions/WpfExtensions.cs
+++ DevilDaggersAssetEditor.Wpf/Extensions/WpfExtensions.cs
@@ -7,21 +7,5 @@ namespace DevilDaggersAssetEditor.Wpf.Extensions
 	{
 		public static Color GetColor(this ChunkInfo chunkInfo)
 			=> Color.FromRgb(chunkInfo.ColorR, chunkInfo.ColorG, chunkInfo.ColorB);
-
-		public static string TrimLeft(this string text, int maxLength)
-		{
-			if (text.Length <= maxLength)
-				return text;
-
-			return text.Substring(text.Length - maxLength).Insert(0, "...");
-		}
-
-		public static string TrimRight(this string text, int maxLength)
-		{
-			if (text.Length <= maxLength)
-				return text;
-
-			return $"{text.Substring(0, maxLength)}...";
-		}
 	}
 }
