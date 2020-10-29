ec9c8fae5ecf1b525590bf33b2667967df7b6df6
Noah Stolk <noah.stolk1997@gmail.com>
2020-10-29 13:04:04 +0100

    Remove DataName from ChunkInfo

--- DevilDaggersAssetEditor/Chunks/ChunkInfo.cs
+++ DevilDaggersAssetEditor/Chunks/ChunkInfo.cs
@@ -7,56 +7,49 @@ namespace DevilDaggersAssetEditor.Chunks
 {
 	public class ChunkInfo
 	{
-		public ChunkInfo(BinaryFileType binaryFileType, Type chunkType, AssetType assetType, string folderName, string dataName)
+		public ChunkInfo(BinaryFileType binaryFileType, Type chunkType, AssetType assetType, string folderName)
 		{
 			BinaryFileType = binaryFileType;
 			ChunkType = chunkType;
 			AssetType = assetType;
 			FolderName = folderName;
-			DataName = dataName;
 		}
 
 		public static ChunkInfo Model { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Dd,
 			chunkType: typeof(ModelChunk),
 			assetType: AssetType.Model,
-			folderName: "Models",
-			dataName: "Model");
+			folderName: "Models");
 
 		public static ChunkInfo Texture { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Dd,
 			chunkType: typeof(TextureChunk),
 			assetType: AssetType.Texture,
-			folderName: "Textures",
-			dataName: "Texture");
+			folderName: "Textures");
 
 		public static ChunkInfo Shader { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Dd | BinaryFileType.Core,
 			chunkType: typeof(ShaderChunk),
 			assetType: AssetType.Shader,
-			folderName: "Shaders",
-			dataName: "Shader");
+			folderName: "Shaders");
 
 		public static ChunkInfo Audio { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Audio,
 			chunkType: typeof(AudioChunk),
 			assetType: AssetType.Audio,
-			folderName: "Audio",
-			dataName: "Audio");
+			folderName: "Audio");
 
 		public static ChunkInfo ModelBinding { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Dd,
 			chunkType: typeof(ModelBindingChunk),
 			assetType: AssetType.ModelBinding,
-			folderName: "Model Bindings",
-			dataName: "Model binding");
+			folderName: "Model Bindings");
 
 		public static ChunkInfo Particle { get; } = new ChunkInfo(
 			binaryFileType: BinaryFileType.Particle,
 			chunkType: typeof(ParticleChunk),
 			assetType: AssetType.Particle,
-			folderName: "Particles",
-			dataName: "Particle");
+			folderName: "Particles");
 
 		public static IReadOnlyList<ChunkInfo> All { get; } = new List<ChunkInfo> { Model, Texture, Shader, Audio, ModelBinding, Particle };
 
@@ -64,6 +57,5 @@ namespace DevilDaggersAssetEditor.Chunks
 		public Type ChunkType { get; }
 		public AssetType AssetType { get; }
 		public string FolderName { get; }
-		public string DataName { get; }
 	}
 }
