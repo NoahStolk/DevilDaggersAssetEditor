82e87bbf9ef985feff7152a72a9d0e9f0e9c1404
Noah Stolk <noah.stolk1997@gmail.com>
2020-11-09 22:29:32 +0100

    Refactor ImportAssets window

---
+++ DevilDaggersAssetEditor.Wpf/Gui/UserControls/ImportDirectoryControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DevilDaggersAssetEditor.User;
+using DevilDaggersAssetEditor.Wpf.Extensions;
+using Ookii.Dialogs.Wpf;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DevilDaggersAssetEditor.Wpf.Gui.UserControls
+{
+	public partial class ImportDirectoryControl : UserControl
+	{
+		private string _directory = UserHandler.Instance.Settings.AssetsRootFolder;
+
+		public ImportDirectoryControl(string header)
+		{
+			InitializeComponent();
+			UpdateGui();
+
+			Header.Content = header;
+		}
+
+		public string Directory => _directory;
+
+		private void BrowseButton_Click(object sender, RoutedEventArgs e)
+			=> SetPath(ref _directory);
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+			=> _directory = TextBox.Text;
+
+		private void UpdateGui()
+			=> TextBox.Text = _directory;
+
+		private void SetPath(ref string path)
+		{
+			if (TrySetPath(out string selectedPath))
+			{
+				path = selectedPath;
+				UpdateGui();
+			}
+		}
+
+		private static bool TrySetPath(out string selectedPath)
+		{
+			VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+			dialog.OpenAssetsRootFolder();
+
+			if (dialog.ShowDialog() != true)
+			{
+				selectedPath = string.Empty;
+				return false;
+			}
+
+			selectedPath = dialog.SelectedPath;
+			return true;
+		}
+	}
+}
--- DevilDaggersAssetEditor.Wpf/Gui/Windows/ImportAssets.xaml.cs
+++ DevilDaggersAssetEditor.Wpf/Gui/Windows/ImportAssets.xaml.cs
@@ -1,114 +1,41 @@
 ﻿using DevilDaggersAssetEditor.Assets;
-using DevilDaggersAssetEditor.User;
-using DevilDaggersAssetEditor.Wpf.Extensions;
-using Ookii.Dialogs.Wpf;
+using DevilDaggersAssetEditor.Wpf.Gui.UserControls;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace DevilDaggersAssetEditor.Wpf.Gui.Windows
 {
 	public partial class ImportAssetsWindow : Window
 	{
-		private string _audioAudioPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _coreShadersPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _ddModelBindingsPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _ddModelsPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _ddShadersPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _ddTexturesPath = UserHandler.Instance.Settings.AssetsRootFolder;
-		private string _particleParticlesPath = UserHandler.Instance.Settings.AssetsRootFolder;
+		private readonly ImportDirectoryControl _audioAudioControl = new ImportDirectoryControl("'audio/Audio' import directory");
+		private readonly ImportDirectoryControl _coreShadersControl = new ImportDirectoryControl("'core/Shaders' import directory");
+		private readonly ImportDirectoryControl _ddModelBindingsControl = new ImportDirectoryControl("'dd/Model Bindings' import directory");
+		private readonly ImportDirectoryControl _ddModelsControl = new ImportDirectoryControl("'dd/Models' import directory");
+		private readonly ImportDirectoryControl _ddShadersControl = new ImportDirectoryControl("'dd/Shaders' import directory");
+		private readonly ImportDirectoryControl _ddTexturesControl = new ImportDirectoryControl("'dd/Textures' import directory");
+		private readonly ImportDirectoryControl _particleParticlesControl = new ImportDirectoryControl("'particle/Particles' import directory");
 
 		public ImportAssetsWindow()
 		{
 			InitializeComponent();
-			UpdateGui();
-		}
 
-		private void UpdateGui()
-		{
-			TextBoxAudioAudio.Text = _audioAudioPath;
-			TextBoxCoreShaders.Text = _coreShadersPath;
-			TextBoxDdModelBindings.Text = _ddModelBindingsPath;
-			TextBoxDdModels.Text = _ddModelsPath;
-			TextBoxDdShaders.Text = _ddShadersPath;
-			TextBoxDdTextures.Text = _ddTexturesPath;
-			TextBoxParticleParticles.Text = _particleParticlesPath;
-		}
-
-		private void BrowseAudioAudioButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _audioAudioPath);
-
-		private void BrowseCoreShadersButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _coreShadersPath);
-
-		private void BrowseDdModelBindingsButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _ddModelBindingsPath);
-
-		private void BrowseDdModelsButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _ddModelsPath);
-
-		private void BrowseDdShadersButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _ddShadersPath);
-
-		private void BrowseDdTexturesButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _ddTexturesPath);
-
-		private void BrowseParticleParticlesButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(ref _particleParticlesPath);
-
-		private void TextBoxAudioAudio_TextChanged(object sender, TextChangedEventArgs e)
-			=> _audioAudioPath = TextBoxAudioAudio.Text;
-
-		private void TextBoxCoreShaders_TextChanged(object sender, TextChangedEventArgs e)
-			=> _coreShadersPath = TextBoxCoreShaders.Text;
-
-		private void TextBoxDdModelBindings_TextChanged(object sender, TextChangedEventArgs e)
-			=> _ddModelBindingsPath = TextBoxDdModelBindings.Text;
-
-		private void TextBoxDdModels_TextChanged(object sender, TextChangedEventArgs e)
-			=> _ddModelsPath = TextBoxDdModels.Text;
-
-		private void TextBoxDdShaders_TextChanged(object sender, TextChangedEventArgs e)
-			=> _ddShadersPath = TextBoxDdShaders.Text;
-
-		private void TextBoxDdTextures_TextChanged(object sender, TextChangedEventArgs e)
-			=> _ddTexturesPath = TextBoxDdTextures.Text;
-
-		private void TextBoxParticleParticles_TextChanged(object sender, TextChangedEventArgs e)
-			=> _particleParticlesPath = TextBoxParticleParticles.Text;
-
-		private void SetPath(ref string path)
-		{
-			if (TrySetPath(out string selectedPath))
-			{
-				path = selectedPath;
-				UpdateGui();
-			}
-		}
-
-		private static bool TrySetPath(out string selectedPath)
-		{
-			VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
-			dialog.OpenAssetsRootFolder();
-
-			if (dialog.ShowDialog() != true)
-			{
-				selectedPath = string.Empty;
-				return false;
-			}
-
-			selectedPath = dialog.SelectedPath;
-			return true;
+			Main.Children.Insert(0, _audioAudioControl);
+			Main.Children.Insert(1, _coreShadersControl);
+			Main.Children.Insert(2, _ddModelBindingsControl);
+			Main.Children.Insert(3, _ddModelsControl);
+			Main.Children.Insert(4, _ddShadersControl);
+			Main.Children.Insert(5, _ddTexturesControl);
+			Main.Children.Insert(6, _particleParticlesControl);
 		}
 
 		private void ImportAssetsButton_Click(object sender, RoutedEventArgs e)
 		{
-			App.Instance.MainWindow!.AudioAudioAssetTabControl.ImportFolder(_audioAudioPath, AssetType.Audio);
-			App.Instance.MainWindow!.CoreShadersAssetTabControl.ImportFolder(_coreShadersPath, AssetType.Shader);
-			App.Instance.MainWindow!.DdModelBindingsAssetTabControl.ImportFolder(_ddModelBindingsPath, AssetType.ModelBinding);
-			App.Instance.MainWindow!.DdModelsAssetTabControl.ImportFolder(_ddModelsPath, AssetType.Model);
-			App.Instance.MainWindow!.DdShadersAssetTabControl.ImportFolder(_ddShadersPath, AssetType.Shader);
-			App.Instance.MainWindow!.DdTexturesAssetTabControl.ImportFolder(_ddTexturesPath, AssetType.Texture);
-			App.Instance.MainWindow!.ParticleParticlesAssetTabControl.ImportFolder(_particleParticlesPath, AssetType.Particle);
+			App.Instance.MainWindow!.AudioAudioAssetTabControl.ImportFolder(_audioAudioControl.Directory, AssetType.Audio);
+			App.Instance.MainWindow!.CoreShadersAssetTabControl.ImportFolder(_coreShadersControl.Directory, AssetType.Shader);
+			App.Instance.MainWindow!.DdModelBindingsAssetTabControl.ImportFolder(_ddModelBindingsControl.Directory, AssetType.ModelBinding);
+			App.Instance.MainWindow!.DdModelsAssetTabControl.ImportFolder(_ddModelsControl.Directory, AssetType.Model);
+			App.Instance.MainWindow!.DdShadersAssetTabControl.ImportFolder(_ddShadersControl.Directory, AssetType.Shader);
+			App.Instance.MainWindow!.DdTexturesAssetTabControl.ImportFolder(_ddTexturesControl.Directory, AssetType.Texture);
+			App.Instance.MainWindow!.ParticleParticlesAssetTabControl.ImportFolder(_particleParticlesControl.Directory, AssetType.Particle);
 
 			Close();
 		}
