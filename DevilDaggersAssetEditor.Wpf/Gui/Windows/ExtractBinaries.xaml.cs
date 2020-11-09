3a5b1bc4c26b440ce3679858080a6a23515bd991
Noah Stolk <noah.stolk1997@gmail.com>
2020-11-09 22:16:10 +0100

    Refactor ExtractBinaries window

--- DevilDaggersAssetEditor.Wpf/Gui/Windows/ExtractBinaries.xaml.cs
+++ DevilDaggersAssetEditor.Wpf/Gui/Windows/ExtractBinaries.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using DevilDaggersAssetEditor.Assets;
 using DevilDaggersAssetEditor.BinaryFileHandlers;
-using DevilDaggersAssetEditor.Extensions;
 using DevilDaggersAssetEditor.User;
 using DevilDaggersAssetEditor.Utils;
 using DevilDaggersAssetEditor.Wpf.Extensions;
-using DevilDaggersAssetEditor.Wpf.Utils;
-using Microsoft.Win32;
+using DevilDaggersAssetEditor.Wpf.Gui.UserControls;
 using Ookii.Dialogs.Wpf;
 using System;
 using System.Collections.Generic;
@@ -15,67 +13,27 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace DevilDaggersAssetEditor.Wpf.Gui.Windows
 {
 	public partial class ExtractBinariesWindow : Window
 	{
-		private string _audioPath = Path.Combine(UserHandler.Instance.Settings.DevilDaggersRootFolder, BinaryFileType.Audio.GetSubfolderName(), nameof(BinaryFileType.Audio).ToLower(CultureInfo.InvariantCulture));
-		private string _corePath = Path.Combine(UserHandler.Instance.Settings.DevilDaggersRootFolder, BinaryFileType.Core.GetSubfolderName(), nameof(BinaryFileType.Core).ToLower(CultureInfo.InvariantCulture));
-		private string _ddPath = Path.Combine(UserHandler.Instance.Settings.DevilDaggersRootFolder, BinaryFileType.Dd.GetSubfolderName(), nameof(BinaryFileType.Dd).ToLower(CultureInfo.InvariantCulture));
-		private string _particlePath = Path.Combine(UserHandler.Instance.Settings.DevilDaggersRootFolder, BinaryFileType.Particle.GetSubfolderName(), nameof(BinaryFileType.Particle).ToLower(CultureInfo.InvariantCulture));
-		private string? _outputPath;
+		private readonly BinaryPathControl _audioControl = new BinaryPathControl("'audio' binary path", BinaryFileType.Audio, AssetType.Audio);
+		private readonly BinaryPathControl _coreControl = new BinaryPathControl("'core' binary path", BinaryFileType.Core, AssetType.Shader);
+		private readonly BinaryPathControl _ddControl = new BinaryPathControl("'dd' binary path", BinaryFileType.Dd, AssetType.Texture);
+		private readonly BinaryPathControl _particleControl = new BinaryPathControl("'particle' binary path", BinaryFileType.Particle, AssetType.Particle);
 
-		private readonly ProgressWrapper _audioProgress;
-		private readonly ProgressWrapper _coreProgress;
-		private readonly ProgressWrapper _ddProgress;
-		private readonly ProgressWrapper _particleProgress;
+		private string? _outputPath;
 
 		public ExtractBinariesWindow()
 		{
 			InitializeComponent();
-			UpdateGui();
 
-			_audioProgress = new ProgressWrapper(
-				new Progress<string>(value => App.Instance.Dispatcher.Invoke(() => ProgressDescriptionAudio.Text = value)),
-				new Progress<float>(value => App.Instance.Dispatcher.Invoke(() => ProgressBarAudio.Value = value)));
-			_coreProgress = new ProgressWrapper(
-				new Progress<string>(value => App.Instance.Dispatcher.Invoke(() => ProgressDescriptionCore.Text = value)),
-				new Progress<float>(value => App.Instance.Dispatcher.Invoke(() => ProgressBarCore.Value = value)));
-			_ddProgress = new ProgressWrapper(
-				new Progress<string>(value => App.Instance.Dispatcher.Invoke(() => ProgressDescriptionDd.Text = value)),
-				new Progress<float>(value => App.Instance.Dispatcher.Invoke(() => ProgressBarDd.Value = value)));
-			_particleProgress = new ProgressWrapper(
-				new Progress<string>(value => App.Instance.Dispatcher.Invoke(() => ProgressDescriptionParticle.Text = value)),
-				new Progress<float>(value => App.Instance.Dispatcher.Invoke(() => ProgressBarParticle.Value = value)));
-
-			ProgressBarAudio.Foreground = new SolidColorBrush(EditorUtils.FromRgbTuple(AssetType.Audio.GetColor()) * 0.5f);
-			ProgressBarCore.Foreground = new SolidColorBrush(EditorUtils.FromRgbTuple(AssetType.Shader.GetColor()) * 0.5f);
-			ProgressBarDd.Foreground = new SolidColorBrush(EditorUtils.FromRgbTuple(AssetType.Texture.GetColor()) * 0.5f);
-			ProgressBarParticle.Foreground = new SolidColorBrush(EditorUtils.FromRgbTuple(AssetType.Particle.GetColor()) * 0.5f);
+			Main.Children.Insert(0, _audioControl);
+			Main.Children.Insert(1, _coreControl);
+			Main.Children.Insert(2, _ddControl);
+			Main.Children.Insert(3, _particleControl);
 		}
-
-		private void UpdateGui()
-		{
-			TextBoxAudio.Text = _audioPath;
-			TextBoxCore.Text = _corePath;
-			TextBoxDd.Text = _ddPath;
-			TextBoxParticle.Text = _particlePath;
-			TextBoxOutput.Text = _outputPath;
-		}
-
-		private void BrowseAudioButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(BinaryFileType.Audio, ref _audioPath);
-
-		private void BrowseCoreButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(BinaryFileType.Core, ref _corePath);
-
-		private void BrowseDdButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(BinaryFileType.Dd, ref _ddPath);
-
-		private void BrowseParticleButton_Click(object sender, RoutedEventArgs e)
-			=> SetPath(BinaryFileType.Particle, ref _particlePath);
 
 		private void BrowseOutputButton_Click(object sender, RoutedEventArgs e)
 		{
@@ -85,51 +43,12 @@ namespace DevilDaggersAssetEditor.Wpf.Gui.Windows
 			if (folderDialog.ShowDialog() == true)
 			{
 				_outputPath = folderDialog.SelectedPath;
-				UpdateGui();
+				TextBoxOutput.Text = _outputPath;
 			}
 		}
-
-		private void TextBoxAudio_TextChanged(object sender, TextChangedEventArgs e)
-			=> _audioPath = TextBoxAudio.Text;
-
-		private void TextBoxCore_TextChanged(object sender, TextChangedEventArgs e)
-			=> _corePath = TextBoxCore.Text;
-
-		private void TextBoxDd_TextChanged(object sender, TextChangedEventArgs e)
-			=> _ddPath = TextBoxDd.Text;
-
-		private void TextBoxParticle_TextChanged(object sender, TextChangedEventArgs e)
-			=> _particlePath = TextBoxParticle.Text;
 
 		private void TextBoxOutput_TextChanged(object sender, TextChangedEventArgs e)
 			=> _outputPath = TextBoxOutput.Text;
-
-		private void SetPath(BinaryFileType binaryFileType, ref string path)
-		{
-			if (TrySetPath(binaryFileType, out string selectedPath))
-			{
-				path = selectedPath;
-				UpdateGui();
-			}
-		}
-
-		private static bool TrySetPath(BinaryFileType binaryFileType, out string selectedPath)
-		{
-			OpenFileDialog openDialog = new OpenFileDialog();
-			string initDir = Path.Combine(UserHandler.Instance.Settings.DevilDaggersRootFolder, binaryFileType.GetSubfolderName());
-			if (UserHandler.Instance.Settings.EnableDevilDaggersRootFolder && Directory.Exists(initDir))
-				openDialog.InitialDirectory = initDir;
-
-			bool? openResult = openDialog.ShowDialog();
-			if (!openResult.HasValue || !openResult.Value)
-			{
-				selectedPath = string.Empty;
-				return false;
-			}
-
-			selectedPath = openDialog.FileName;
-			return true;
-		}
 
 		private async void ExtractBinaries_Click(object sender, RoutedEventArgs e)
 		{
@@ -137,10 +56,10 @@ namespace DevilDaggersAssetEditor.Wpf.Gui.Windows
 
 			await Task.WhenAll(new List<Task>
 			{
-				ExtractBinary(BinaryFileType.Audio, _audioPath, _outputPath, _audioProgress),
-				ExtractBinary(BinaryFileType.Core, _corePath, _outputPath, _coreProgress),
-				ExtractBinary(BinaryFileType.Dd, _ddPath, _outputPath, _ddProgress),
-				ExtractBinary(BinaryFileType.Particle, _particlePath, _outputPath, _particleProgress),
+				ExtractBinary(BinaryFileType.Audio, _audioControl.BinaryPath, _outputPath, _audioControl.Progress),
+				ExtractBinary(BinaryFileType.Core, _coreControl.BinaryPath, _outputPath, _coreControl.Progress),
+				ExtractBinary(BinaryFileType.Dd, _ddControl.BinaryPath, _outputPath, _ddControl.Progress),
+				ExtractBinary(BinaryFileType.Particle, _particleControl.BinaryPath, _outputPath, _particleControl.Progress),
 			});
 
 			ButtonExtractBinaries.IsEnabled = true;
