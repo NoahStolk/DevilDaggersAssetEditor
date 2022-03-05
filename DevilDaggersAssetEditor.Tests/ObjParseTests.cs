8019820f70bf544055b9c566993a051834638230
Noah Stolk <noah.stolk@gmail.com>
2022-03-05 10:41:17 +0100

    Add more test meshes

--- DevilDaggersAssetEditor.Tests/ObjParseTests.cs
+++ DevilDaggersAssetEditor.Tests/ObjParseTests.cs
@@ -1,5 +1,6 @@
 using DevilDaggersAssetEditor.Binaries.Chunks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
 namespace DevilDaggersAssetEditor.Tests;
@@ -11,8 +12,17 @@ public class ObjParseTests
 	[DataRow("3dsMax-claw.obj")]
 	[DataRow("3dsMax-hand.obj")]
 	[DataRow("Wings3d-cylinder.obj")]
+	[DataRow("Wings3d-cube.obj")]
 	public void ParseObj(string fileName)
 	{
 		ModelChunk.ReadObj(Path.Combine("Resources", fileName), out _, out _, out _, out _);
+	}
+
+	[DataTestMethod]
+	[DataRow("Wings3d-cube-invalid-face.obj")]
+	[DataRow("Wings3d-cube-no-uv.obj")]
+	public void ParseInvalidObj(string fileName)
+	{
+		Assert.ThrowsException<Exception>(() => ModelChunk.ReadObj(Path.Combine("Resources", fileName), out _, out _, out _, out _));
 	}
 }
