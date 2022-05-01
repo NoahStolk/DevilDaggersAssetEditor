a26ada7eebe170e17d146f0f2e119abde2aaf26a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-05-01 12:23:03 +0200

    Fix URL encoding when downloading screenshots

--- DevilDaggersAssetEditor.Wpf/Caching/ModScreenshotCache.cs
+++ DevilDaggersAssetEditor.Wpf/Caching/ModScreenshotCache.cs
@@ -22,7 +22,7 @@ public sealed class ModScreenshotCache
 		if (_cache.ContainsKey(key))
 			return _cache[key];
 
-		BitmapImage image = new(new Uri($"https://devildaggers.info/api/mod-screenshots?modName={modName}&fileName={screenshotFileName}"));
+		BitmapImage image = new(new Uri($"https://devildaggers.info/api/mod-screenshots?modName={Uri.EscapeDataString(modName)}&fileName={Uri.EscapeDataString(screenshotFileName)}"));
 		_cache.Add(key, image);
 		return image;
 	}
