4ae1467eb4cfa0f0560ab33cc515a2df5310b0cb
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-17 20:33:07 +0100

    Update to latest API spec

--- DevilDaggersAssetEditor/Utils/AssemblyUtils.cs
+++ DevilDaggersAssetEditor/Utils/AssemblyUtils.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
@@ -7,10 +5,6 @@ namespace DevilDaggersAssetEditor.Utils;
 
 public static class AssemblyUtils
 {
-	public static Assembly CurrentAssembly => Assembly.GetExecutingAssembly();
-
-	internal static Version LocalVersion => Version.Parse(FileVersionInfo.GetVersionInfo(CurrentAssembly.Location).FileVersion ?? throw new("Could not get file version from current assembly."));
-
 	public static Stream GetContentStream(string relativeContentName)
-		=> CurrentAssembly.GetManifestResourceStream($"DevilDaggersAssetEditor.Content.{relativeContentName}") ?? throw new($"Could not retrieve content stream '{relativeContentName}'.");
+		=> Assembly.GetExecutingAssembly().GetManifestResourceStream($"DevilDaggersAssetEditor.Content.{relativeContentName}") ?? throw new($"Could not retrieve content stream '{relativeContentName}'.");
 }
